51bf9a62b65c02781aa7c793498687dd1ab8909e
Andrew Bustos <infernious@live.com>
2024-12-01 20:07:43 -0800

    added home button to 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,22 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button"
+
 export default function Custom404() {
-return(
-  <>
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-      <div className="text-center p-8 bg-white bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg">
-        <p className="text-xl text-white mb-6">
-         we're all chill here.
-        </p>
-        <img src="https://utfs.io/f/tohTttgIkwLBx0qqYnRJ1Lhx6lKibDfad9rR3gnq75MEO0mG" className="h-72 mx-auto mb-4 text-yellow-300" />
-        router.push("/tatebot")
+  const router = useRouter()
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+        <div className="text-center p-8 bg-white bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg">
+          <p className="text-xl text-white mb-6">we're all chill here.</p>
+          <img
+            src="https://utfs.io/f/tohTttgIkwLBx0qqYnRJ1Lhx6lKibDfad9rR3gnq75MEO0mG"
+            className="h-72 mx-auto mb-4 text-yellow-300"
+          />
+          <Button onClick={() => router.push("/")}>Home</Button>
+        </div>
       </div>
-    </div>
-  </>
-)    
-  }
+    </>
+  )
+}
